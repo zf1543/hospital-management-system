6f98d67ef82d158762591177f41538d8b48a8320
zf1543 <fanzhong1543@gmail.com>
2025-12-15 09:02:45 +0800

    Add files via upload

---
+++ StyleSelector.h
@@ -0,0 +1,21 @@
+﻿#ifndef STYLESELECTOR_H
+#define STYLESELECTOR_H
+
+#include <QFile>
+#include <QApplication>
+
+//样式选择器
+class StyleSelector
+{
+public:
+    StyleSelector();
+    //静态函数，载入该路径的qss样式
+    static void setStyle(const QString &style) {
+        QFile qss(style);
+        qss.open(QFile::ReadOnly);
+        qApp->setStyleSheet(qss.readAll());
+        qss.close();
+    }
+};
+
+#endif // STYLESELECTOR_H
