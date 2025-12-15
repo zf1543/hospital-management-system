5681b685c2b3cd401b88ad10fccd03a91e254cab
zf1543 <fanzhong1543@gmail.com>
2025-12-15 09:06:34 +0800

    Add files via upload

---
+++ FilterDialog.cpp
@@ -0,0 +1,61 @@
+﻿#include "FilterDialog.h"
+#include "ui_FilterDialog.h"
+
+FilterDialog::FilterDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::FilterDialog)
+{
+    ui->setupUi(this);
+
+    ui->TargetBox->clear();
+    // 重新添加一次选项即可
+    ui->TargetBox->addItems({"患者ID", "姓名", "性别", "年龄", "电话", "诊断结果"});
+
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, [=](){ qDebug("ok or save");}); //绑定
+    connect(ui->buttonBox, &QDialogButtonBox::rejected, this, [=](){ qDebug("cancel"); });
+}
+
+FilterDialog::~FilterDialog()
+{
+    delete ui;
+}
+
+QString FilterDialog::getKey()
+{
+    return this->ui->KeyEdit->text();
+}
+
+QString FilterDialog::getTarget()
+{
+    return this->ui->TargetBox->currentText();
+}
+
+int FilterDialog::getTargetIndex()
+{
+    return this->ui->TargetBox->currentIndex();
+}
+
+QString FilterDialog::getType()
+{
+    return this->ui->TypeBox->currentText();
+}
+
+bool FilterDialog::isRegex()
+{
+    return this->ui->RegxBox->checkState();
+}
+
+bool FilterDialog::isWild()
+{
+    return this->ui->WildBoix->checkState();
+}
+
+QString FilterDialog::getTargetText()
+{
+    return ui->TargetBox->currentText(); // 返回下拉框选中的文字，如“姓名”
+}
+
+bool FilterDialog::isCaseSensitive()
+{
+    return this->ui->CaseBox->checkState();
+}
---
+++ config.h
@@ -0,0 +1,9 @@
+﻿#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <QString>
+
+//配置参数
+const QString USERNAME = "zhongfan";                      //用户名
+const QString PASSWORD = "123456";                   //密码
+#endif // CONFIG_H
