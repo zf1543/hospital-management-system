8ac42254947055d036731d05499854fe82259213
zf1543 <fanzhong1543@gmail.com>
2025-12-15 09:04:37 +0800

    Add files via upload

---
+++ SortDialog.cpp
@@ -0,0 +1,27 @@
+﻿#include "SortDialog.h"
+#include "ui_SortDialog.h"
+
+SortDialog::SortDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SortDialog)
+{
+    ui->setupUi(this);
+
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, [=](){ qDebug("ok or save");}); //绑定
+    connect(ui->buttonBox, &QDialogButtonBox::rejected, this, [=](){ qDebug("cancel"); });
+}
+
+SortDialog::~SortDialog()
+{
+    delete ui;
+}
+
+int SortDialog::getSortTarget()
+{
+    return this->ui->sortTarget->currentIndex();
+}
+
+int SortDialog::getSortOrder()
+{
+    return this->ui->sortOrder->currentIndex();
+}
